0cfe9bd43f7d8cec1d313d2c13e7702069674922
Wouter Verhelst <wouter.verhelst@fedict.be>
2014-12-04 15:10:28 +0100

    Initial commit
    Add a (somewhat working) robot firmware.
    
    This version supports the following commands (case-insensitive):
    - 'p': park the arm (so it is between the "insert" and "eject" points)
    - 'e': eject the card
    - 'i': insert the card
    - 's': stop movement
    
    The robot will report the location of the arm when movement stops, as
    "ejected", "parked", or "inserted".

---
+++ eid_arduino/eid_arduino.ino
@@ -0,0 +1,168 @@
+int inSensor = 5;
+int ejSensor = 2;
+int engineLeft = 3;
+int engineRight = 11;
+int engineSensor = 7;
+int led = 13;
+int turnSpeed = 255;
+enum tstate {
+  STOP = 0,
+  EJECT = 1,
+  INSERT = 2,
+  PARK = 3,
+} target = INSERT;
+enum cstate {
+  STOPPED = 0,
+  EJECTED = 1,
+  INSERTED = 2,
+  PARKED = 3,
+  MOVING_LEFT = 4,
+  MOVING_RIGHT = 5,
+  ERROR = 9,
+} curr = STOPPED;
+void(*insert)();
+void(*eject)();
+int printed = 0;
+
+void turnLeft() {
+  analogWrite(engineRight, 0);
+  analogWrite(engineLeft, turnSpeed);
+  curr = MOVING_LEFT;
+}
+
+void turnRight() {
+  analogWrite(engineLeft, 0);
+  analogWrite(engineRight, turnSpeed);
+  curr = MOVING_RIGHT;
+}
+
+void stopMove() {
+  analogWrite(engineLeft, 0);
+  analogWrite(engineRight, 0);
+  curr = STOPPED;
+}
+
+void setup() {
+  pinMode(inSensor, INPUT_PULLUP);
+  pinMode(ejSensor, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+  analogWrite(engineLeft, 0);
+  analogWrite(engineRight, 0);
+  while(!Serial) {
+    ;
+  }
+  Serial.begin(9600);
+  turnLeft();
+  while(curr != INSERTED && curr != EJECTED) {
+    if(digitalRead(ejSensor) == HIGH) {
+      stopMove();
+      curr = EJECTED;
+      eject = turnLeft;
+      insert = turnRight;
+    }
+    if(digitalRead(inSensor) == HIGH) {
+      stopMove();
+      curr = INSERTED;
+      insert = turnRight;
+      eject = turnLeft;
+    }
+  }
+  Serial.println("READY.");
+}
+
+void loop() {
+  int inSensorState;
+  int ejSensorState;
+  
+  if(target != curr) {
+    digitalWrite(led, HIGH);
+    printed = 0;
+    switch(target) {
+      case STOP:
+        stopMove();
+        break;
+      case EJECT:
+        eject();
+        break;
+      case INSERT:
+        insert();
+        break;
+      case PARK:
+        if(curr == INSERTED) {
+          eject();
+        }
+        if(curr == EJECTED) {
+          insert();
+        }
+        break;
+    }
+    if(curr == ERROR) {
+      Serial.println("ERROR!");
+    }
+  } else {
+    digitalWrite(led, LOW);
+    if(!printed) {
+      printed = 1;
+      switch(curr) {
+        case STOPPED:
+          Serial.println("stop");
+          printed = 0;
+          break;
+        case EJECTED:
+          Serial.println("ejected");
+          break;
+        case INSERTED:
+          Serial.println("inserted");
+          break;
+        case PARKED:
+          Serial.println("parked");
+          break;
+      }
+    }
+  }
+
+  inSensorState = digitalRead(inSensor);
+  ejSensorState = digitalRead(ejSensor);
+  if(ejSensorState == HIGH && target == EJECT) {
+    stopMove();
+  }
+  if(inSensorState == HIGH && target == INSERT) {
+    stopMove();
+  }
+  if(inSensorState == LOW && ejSensorState == LOW && target == PARK) {
+    stopMove();
+  }
+  if(curr == STOPPED) {
+    if(inSensorState == HIGH) {
+      curr = INSERTED;
+    } else if(ejSensorState == HIGH) {
+      curr = EJECTED;
+    } else {
+      curr = PARKED;
+    }
+  }
+  if(target == STOP) {
+    target = (tstate)curr;
+  }
+  if(Serial.available() > 0) {
+    switch(Serial.read()) {
+      case 'i':
+      case 'I':
+        target = INSERT;
+        break;
+      case 'e':
+      case 'E':
+        target = EJECT;
+        break;
+      case 's':
+      case 'S':
+        target = STOP;
+        break;
+      case 'p':
+      case 'P':
+        target = PARK;
+        break;
+    }
+    printed = 0;
+  }
+}
